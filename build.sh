6476c6fac54052e111780f60cf5dd9d8a6c76019
Michael Quinlan <mquinlan@gigsmart.com>
2022-06-10 15:44:05 -0600

    Modifying build script to accept different platforms

--- build.sh
+++ build.sh
@@ -12,17 +12,32 @@ images=(
   postgres:14-alpine
 )
 
+platform=${PLATFORM:-"linux/arm64"}
+image_name=${IMAGE_NAME:-"mjquinlan2000/postgis-arm64"}
+
 for image in ${images[@]}; do
   for version in ${gis_versions[@]}; do
     if [[ $version = "3.0.5" ]] && [[ $image = "postgres:14-alpine" ]]; then continue; fi
+    default_tag=`echo $image | sed s/:/-/g`-postgis-$version
+    tag=${TAG:-$default_tag}
 
-    tag=`echo $image | sed s/:/-/g`-postgis-$version
-    docker buildx build --platform linux/amd64,linux/arm64 --pull \
+    echo "Starting build..."
+    echo "IMAGE: $image"
+    echo "POSTGIS VERSION: $version"
+    echo "PLATFORM: $platform"
+    echo "IMAGE NAME: $image_name"
+    echo "TAG: $tag"
+
+    docker build --platform $platform --pull \
       --build-arg VERSION=$version \
       --build-arg IMAGE=$image \
-      -t mjquinlan2000/postgis:$tag \
-      --push .
+      -t $image_name:$tag .
 
-    echo $tag
+    if [[ $PUSH = true ]]; then
+      echo "Pushing image..."
+      docker push $image_name:$tag
+    fi
+
+    echo "Done!"
   done
 done
